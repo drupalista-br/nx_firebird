5796a4a2813e1ecbee9647e5e297c349fe9a353b
Francisco Luz <franciscoferreiraluz@yahoo.com.au>
2015-03-25 13:20:06 -0400

    Fixed passthru command.

--- app-update.php
+++ app-update.php
@@ -8,8 +8,8 @@ if (file_exists("tmp/commit.txt")) {
 }
 
 if ($commit_last != $commit_current) {
-  passthru("git pull origin master &&
-		   git checkout $commit_current");
+  passthru("git pull origin master");
+  passthru("git checkout $commit_current");
 
   if (!file_exists("tmp")) {
 	mkdir("tmp", 0777, true);
