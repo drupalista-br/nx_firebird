589883e4e130fbb6f294c47a386766daf0de078d
Francisco Luz <franciscoferreiraluz@yahoo.com.au>
2015-03-25 12:50:15 -0400

    ...

--- app-update.php
+++ app-update.php
@@ -1,7 +1,7 @@
 <?php
 
 $commit_last = '';
-$commit_current = file_get_contents("app-update.commit");
+$commit_current = file_get_contents("https://raw.githubusercontent.com/drupalista-br/nx_firebird/master/app-update.commit");
 
 if (file_exists("tmp/commit.txt")) {
   $commit_last = file_get_contents("tmp/commit.txt");
@@ -10,4 +10,5 @@ if (file_exists("tmp/commit.txt")) {
 if ($commit_last != $commit_current) {
   passthru("git pull origin master &&
 		   git checkout $commit_current");
+  file_put_contents("tmp/commit.txt", $commit_current);
 }
