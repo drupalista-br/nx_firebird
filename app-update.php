55721026526b6ca51621af2e9ab78cbcf5f163a5
Francisco Luz <franciscoferreiraluz@yahoo.com.au>
2015-03-25 12:58:02 -0400

    Fixed tmp folder does not exist.

--- app-update.php
+++ app-update.php
@@ -10,5 +10,9 @@ if (file_exists("tmp/commit.txt")) {
 if ($commit_last != $commit_current) {
   passthru("git pull origin master &&
 		   git checkout $commit_current");
+
+  if (!file_exists($tmp_folder)) {
+	mkdir($tmp_folder, 0777, true);
+  }
   file_put_contents("tmp/commit.txt", $commit_current);
 }
