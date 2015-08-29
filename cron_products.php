eed77058fc22f2b2a321636a80aad50f800efd5c
Francisco Luz <franciscoferreiraluz@yahoo.com.au>
2015-08-28 23:08:45 -0400

    Replaced tabs with 2 spaces.

--- cron_products.php
+++ cron_products.php
@@ -1,6 +1,7 @@
 <?php
-use Ecocentauro\tools as tools,
-	Zend\Config\Writer\Ini as IniWriter;
+use
+  Ecocentauro\tools as tools,
+  Zend\Config\Writer\Ini as IniWriter;
 
 setlocale(LC_CTYPE, 'pt_BR');
 
@@ -24,7 +25,7 @@ if (!empty($products->last_cron_difference)) {
   // Get NX Wsclient Produto Folder.
   $nx_dados_folder = "{$products->config['nx_wsclient']['root_folder']}{$separator}dados";
   if (isset($nx_wsclient_config['pastas']['dados']) && file_exists($nx_wsclient_config['pastas']['dados'])) {
-	$nx_dados_folder = $nx_wsclient_config['pastas']['dados'];
+    $nx_dados_folder = $nx_wsclient_config['pastas']['dados'];
   }
   $nx_produto_folder = "$nx_dados_folder{$separator}produto";
 
@@ -32,38 +33,38 @@ if (!empty($products->last_cron_difference)) {
 
   // Deletions | Set stock to zero.
   if (isset($diff['deleted'])) {
-	foreach($diff['deleted'] as $product_id => $product) {
-	  $product['qtde_em_estoque'] = 0;
-	  $product['preco'] = number_format($product['preco'], 2, '', '');
-	  $product['cod_cidade'] = $nx_cod_cidade;
-	  $product['ativo'] = 0;
+    foreach($diff['deleted'] as $product_id => $product) {
+      $product['qtde_em_estoque'] = 0;
+      $product['preco'] = number_format($product['preco'], 2, '', '');
+      $product['cod_cidade'] = $nx_cod_cidade;
+      $product['ativo'] = 0;
 
-	  $ini_writter->toFile("$nx_produto_folder{$separator}$product_id.txt", $product);
-	}
-	unset($diff['deleted']);
+      $ini_writter->toFile("$nx_produto_folder{$separator}$product_id.txt", $product);
+    }
+    unset($diff['deleted']);
   }
 
   // Insertions and Updates.
   foreach($diff as $product_id => $product) {
-	$product['preco'] = number_format($product['preco'], 2, '', '');
-	$product['cod_cidade'] = $nx_cod_cidade;
-	$product['nome'] = str_replace('"', '', $product['nome']);
+    $product['preco'] = number_format($product['preco'], 2, '', '');
+    $product['cod_cidade'] = $nx_cod_cidade;
+    $product['nome'] = str_replace('"', '', $product['nome']);
 
-	switch($product['ativo']) {
-	  case 'S':
-		$product['ativo'] = 1;
-	  break;
-	  case 'N':
-		$product['ativo'] = 0;
-	  break;
-	}
+    switch($product['ativo']) {
+      case 'S':
+      $product['ativo'] = 1;
+      break;
+      case 'N':
+      $product['ativo'] = 0;
+      break;
+    }
 
-	// Make sure no negative value is sent for stock field.
-	if ($product['qtde_em_estoque'] < 0) {
-	  $product['qtde_em_estoque'] = 0;
-	}
+    // Make sure no negative value is sent for stock field.
+    if ($product['qtde_em_estoque'] < 0) {
+      $product['qtde_em_estoque'] = 0;
+    }
 
-	$ini_writter->toFile("$nx_produto_folder{$separator}$product_id.txt", $product);
+    $ini_writter->toFile("$nx_produto_folder{$separator}$product_id.txt", $product);
   }
 
   // Call NX WSClient sync.
--- tools.php
+++ tools.php
@@ -3,46 +3,49 @@ namespace Ecocentauro;
 setlocale(LC_CTYPE, 'pt_BR');
 
 class tools {
-  protected $cwd;
-  public $config;
-  protected $firebird_credentials;
-  protected $db_connection;
-  public $query_result;
-  public $last_cron_difference;
+  protected
+    $cwd,
+    $firebird_credentials,
+    $db_connection;
+
+  public
+    $config,
+    $query_result,
+    $last_cron_difference;
 
   /**
    * Loads the config.ini.
    */
   public function __construct() {
-	$cwd = pathinfo(__DIR__);
-	$this->cwd = $cwd = $cwd['dirname'] . DIRECTORY_SEPARATOR . $cwd['basename'];
-	$config_file = $cwd . DIRECTORY_SEPARATOR . "config.ini";
-	$this->config = $config = parse_ini_file($config_file, TRUE);
+    $cwd = pathinfo(__DIR__);
+    $this->cwd = $cwd = $cwd['dirname'] . DIRECTORY_SEPARATOR . $cwd['basename'];
+    $config_file = $cwd . DIRECTORY_SEPARATOR . "config.ini";
+    $this->config = $config = parse_ini_file($config_file, TRUE);
 
-	$nx_wsclient_root_folder = $config['nx_wsclient']['root_folder'];
-	$nx_wsclient_autoload_file = $nx_wsclient_root_folder . DIRECTORY_SEPARATOR . $config['nx_wsclient']['autoload_file'];
+    $nx_wsclient_root_folder = $config['nx_wsclient']['root_folder'];
+    $nx_wsclient_autoload_file = $nx_wsclient_root_folder . DIRECTORY_SEPARATOR . $config['nx_wsclient']['autoload_file'];
 
-	if (file_exists($nx_wsclient_autoload_file)) {
-	  include_once $nx_wsclient_autoload_file;
-	}
+    if (file_exists($nx_wsclient_autoload_file)) {
+      include_once $nx_wsclient_autoload_file;
+    }
   }
 
   /**
    * Creates a firebird connection resource.
    */
   private function firebird_db_connect() {
-	$db = $this->config['firebird_credentials']['db'];
-	$username = $this->config['firebird_credentials']['username'];
-	$password = $this->config['firebird_credentials']['password'];
+    $db = $this->config['firebird_credentials']['db'];
+    $username = $this->config['firebird_credentials']['username'];
+    $password = $this->config['firebird_credentials']['password'];
 
-	$this->db_connection = @ibase_connect($db, $username, $password, 'ISO8859_1');
+    $this->db_connection = @ibase_connect($db, $username, $password, 'ISO8859_1');
 
-	if (ibase_errmsg()) {
-	  $msg = ibase_errmsg();
-	  print "SEND AN EMAIL $msg\n";
+    if (ibase_errmsg()) {
+      $msg = ibase_errmsg();
+      print "SEND AN EMAIL $msg\n";
 
-	  throw new \Exception($msg);
-	}
+      throw new \Exception($msg);
+    }
   }
 
   /**
@@ -53,52 +56,52 @@ class tools {
    *   i.e. [firebird_$table]
    */
   public function firebird_config_query($table) {
-	if (!is_resource($this->db_connection)) {
-	  $this->firebird_db_connect();
-	}
+    if (!is_resource($this->db_connection)) {
+      $this->firebird_db_connect();
+    }
 
-	if (isset($this->config["firebird_$table"]['table_name'])) {
-	  $table_name = $this->config["firebird_$table"]['table_name'];
-	  $table_fields = $this->config["firebird_$table"]['fields'];
-	  $field_id = $this->config["firebird_$table"]['field_id'];
-  
-	  $query_fields = '';
-	  foreach ($table_fields as $table_field_name => $nx_table_field_name) {
-		if (!empty($query_fields)) {
-		  $query_fields .= ', ';
-		}
-		$query_fields .= $table_field_name;
-	  }
-  
-	  $query = "SELECT $query_fields FROM $table_name";
-	  $query = @ibase_query($this->db_connection, $query);
+    if (isset($this->config["firebird_$table"]['table_name'])) {
+      $table_name = $this->config["firebird_$table"]['table_name'];
+      $table_fields = $this->config["firebird_$table"]['fields'];
+      $field_id = $this->config["firebird_$table"]['field_id'];
 
-	  if (ibase_errmsg()) {
-		$test = ibase_errmsg();
-		print "SEND AN EMAIL $test\n";
-	  }
+      $query_fields = '';
+      foreach ($table_fields as $table_field_name => $nx_table_field_name) {
+        if (!empty($query_fields)) {
+          $query_fields .= ', ';
+        }
+        $query_fields .= $table_field_name;
+      }
 
-  	  $result = array();
+      $query = "SELECT $query_fields FROM $table_name";
+      $query = @ibase_query($this->db_connection, $query);
 
-	  $row = 0;
-	  while ($row_temp = ibase_fetch_assoc($query)) {
-		foreach($row_temp as $table_field_name => $table_field_value) {
-		  $nx_table_field_name = $table_fields[$table_field_name];
-		  $field_id_value = $row_temp[$field_id];
+      if (ibase_errmsg()) {
+        $test = ibase_errmsg();
+        print "SEND AN EMAIL $test\n";
+      }
 
-		  $result[$field_id_value][$nx_table_field_name] = utf8_encode(trim($table_field_value)); //iconv('UTF-8', 'ASCII//TRANSLIT', trim($table_field_value)); //iconv('UTF-8','UTF-8//TRANSLIT', strtr(trim($table_field_value), $replaces));
-		}
-		$row++;
-	  }
-	  $this->query_result = $result;
-	}
-	else {
-	  print "SEND AN EMAIL\n";
-	}
+      $result = array();
 
-	if (is_resource($this->db_connection)) {
-	  ibase_close($this->db_connection);
-	}
+      $row = 0;
+      while ($row_temp = ibase_fetch_assoc($query)) {
+        foreach($row_temp as $table_field_name => $table_field_value) {
+          $nx_table_field_name = $table_fields[$table_field_name];
+          $field_id_value = $row_temp[$field_id];
+
+          $result[$field_id_value][$nx_table_field_name] = utf8_encode(trim($table_field_value)); //iconv('UTF-8', 'ASCII//TRANSLIT', trim($table_field_value)); //iconv('UTF-8','UTF-8//TRANSLIT', strtr(trim($table_field_value), $replaces));
+        }
+        $row++;
+      }
+      $this->query_result = $result;
+    }
+    else {
+      print "SEND AN EMAIL\n";
+    }
+
+    if (is_resource($this->db_connection)) {
+      ibase_close($this->db_connection);
+    }
   }
 
   /**
@@ -109,16 +112,16 @@ class tools {
    *   The name of the file to be saved in the tmp folder.
    */
   public function store_query_result($file_name) {
-	$cwd = $this->cwd;
-	$tmp_folder = $cwd . DIRECTORY_SEPARATOR . 'tmp';
-	$tmp_file_path = $tmp_folder . DIRECTORY_SEPARATOR . $file_name;
+    $cwd = $this->cwd;
+    $tmp_folder = $cwd . DIRECTORY_SEPARATOR . 'tmp';
+    $tmp_file_path = $tmp_folder . DIRECTORY_SEPARATOR . $file_name;
 
-	$file_content = json_encode($this->query_result);
+    $file_content = json_encode($this->query_result);
 
-	if (!file_exists($tmp_folder)) {
-	  mkdir($tmp_folder, 0777, true);
-	}
-	file_put_contents($tmp_file_path, $file_content);
+    if (!file_exists($tmp_folder)) {
+      mkdir($tmp_folder, 0777, true);
+    }
+    file_put_contents($tmp_file_path, $file_content);
   }
 
   /**
@@ -130,41 +133,41 @@ class tools {
    *   The file name containing last cron content.
    */
   public function compare_last_cron_result($last_cron_file_name) {
-	$cwd = $this->cwd;
-	$tmp_folder = $cwd . DIRECTORY_SEPARATOR . 'tmp';
-	$tmp_file_path = $tmp_folder . DIRECTORY_SEPARATOR . $last_cron_file_name;
-	$content_last_cron = array();
-	$content_current_cron = $this->query_result;
-	$content_difference = array();
+    $cwd = $this->cwd;
+    $tmp_folder = $cwd . DIRECTORY_SEPARATOR . 'tmp';
+    $tmp_file_path = $tmp_folder . DIRECTORY_SEPARATOR . $last_cron_file_name;
+    $content_last_cron = array();
+    $content_current_cron = $this->query_result;
+    $content_difference = array();
 
-	//unset($content_current_cron[37]);
+    //unset($content_current_cron[37]);
 
-	if (file_exists($tmp_file_path)) {
-	  $content_last_cron = (array) json_decode(file_get_contents($tmp_file_path), TRUE);
-	}
+    if (file_exists($tmp_file_path)) {
+      $content_last_cron = (array) json_decode(file_get_contents($tmp_file_path), TRUE);
+    }
 
-	foreach($content_current_cron as $content_id => $content_row) {
-	  // New content.
-	  if (!array_key_exists($content_id, $content_last_cron)) {
-		$content_difference[$content_id] = $content_row;
-	  }
-	  else {
-		// Content update.
-		$diff = array_diff($content_current_cron[$content_id], $content_last_cron[$content_id]);
-		if (!empty($diff)) {
-		  $content_difference[$content_id] = $content_row;
-		}
-	  }
-	}
+    foreach($content_current_cron as $content_id => $content_row) {
+      // New content.
+      if (!array_key_exists($content_id, $content_last_cron)) {
+        $content_difference[$content_id] = $content_row;
+      }
+      else {
+      // Content update.
+      $diff = array_diff($content_current_cron[$content_id], $content_last_cron[$content_id]);
+      if (!empty($diff)) {
+        $content_difference[$content_id] = $content_row;
+      }
+      }
+    }
 
-	// Content deleted.
-	$rows_deleted = array_diff_key($content_last_cron, $content_current_cron);
-	if (!empty($rows_deleted)) {
-	  foreach($rows_deleted as $content_id => $content_row) {
-		$content_difference['deleted'][$content_id] = $content_row;
-	  }
-	}
+    // Content deleted.
+    $rows_deleted = array_diff_key($content_last_cron, $content_current_cron);
+    if (!empty($rows_deleted)) {
+      foreach($rows_deleted as $content_id => $content_row) {
+        $content_difference['deleted'][$content_id] = $content_row;
+      }
+    }
 
-	$this->last_cron_difference = $content_difference;
+    $this->last_cron_difference = $content_difference;
   }
 }
